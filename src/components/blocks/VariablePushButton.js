45297f1072f89dd58051539b97e9ea446194ec1c
Marcos Inonan <minonan@uw.edu>
2022-10-25 13:15:35 -0400

    Input Push Button Widget added

---
+++ src/components/blocks/VariablePushButton.js
@@ -0,0 +1,92 @@
+import $ from 'jquery';
+import useScript from '../../useScript';
+
+export function ReliaVariablePushButton($divElement, deviceIdentifier, blockIdentifier) {
+	var self = this;
+
+	self.$div = $divElement;
+
+	self.$div.html(
+	    "<h3>Variable CheckBox " + blockIdentifier + " of " + deviceIdentifier + "</h3>" +
+	    "<div>" +
+		    "<input type=\"submit\" name=\"checkout\" class=\"button press-button\" value=\"On/Off\"> <br>" +
+	    "</div>"
+	);
+
+	self.url = window.API_BASE_URL + "data/current/devices/" + deviceIdentifier + "/blocks/" + blockIdentifier;
+
+	self.stateInitialized = false;
+	self.value = false;
+	self.flagPressedUnpressed=true;
+
+
+	self.$div.find(".press-button").click(function() {
+		if (self.flagPressedUnpressed==true) 
+			self.flagPressedUnpressed=false;
+		else self.flagPressedUnpressed=true;
+	
+		console.log("on checkbox change:", self.value);
+
+		$.ajax({
+			type: "POST",
+			url: self.url, 
+			data: JSON.stringify({
+				"value": self.value
+			}),
+			contentType: "application/json",
+			dataType: "json"
+		}).done(function () {
+			// TBD
+		});
+	});
+	self.changeCheckBox();
+
+	self.$checkbox.change(self.changeCheckBox);
+
+	self.redraw = function () {
+		$.get(self.url).done(function (data) {
+			setTimeout(function () {
+				self.redraw();
+			});
+
+			if (!data.success) {
+				console.log("Error: " + data.message);
+				return;
+			}
+
+			if (data.data == null) {
+				console.log("No data");
+				return;
+			}
+
+			console.log(data.data);
+
+			var params = data.data.params;
+			self.choices = params.choices;
+
+			if (!self.stateInitialized) {
+				if (params.state) {
+					self.prop('checked', true);
+				} else {
+					self.prop('checked', false);
+				}
+				self.stateInitialized = true;
+			}
+		});
+	};
+
+	$.ajax({
+		type: "POST",
+		url: self.url, 
+		data: JSON.stringify({
+			"forceUploadData": true
+		}),
+		contentType: "application/json",
+		dataType: "json"
+	}).done(function () {
+		// TBD
+	});
+
+}
+
+export default ReliaVariablePushButton;

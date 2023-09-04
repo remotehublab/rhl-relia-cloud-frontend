dc62ae11694393372b70b6cd4707e5cf2a9a3249
Pedro <ppedrolia@gmail.com>
2023-09-04 16:11:22 -0300

    Work on UI/UX: * made components work more intuitively * added multiple file selection * very basic css

--- src/Loader.js
+++ src/Loader.js
@@ -9,56 +9,66 @@
  */
 import React, { useState } from 'react';
 
+import './Loader.css';
 /**
  * Uploader Component
  * @param {Array} uploadedFiles - An array of uploaded files.
  * @param {Function} setUploadedFiles - A function to update the uploaded files.
  *
- * @returns {JSX.Element} The rendered Selector component.
+ * @returns {JSX.Element} The rendered Uploader component.
  */
-function Uploader({ uploadedFiles, setUploadedFiles }) {
-  const [selectedFile, setSelectedFile] = useState(null);
+function Uploader({ uploadedFiles, setUploadedFiles ,tableIsVisible, setTableIsVisible }) {
+  const [selectedFiles, setSelectedFiles] = useState([]);
 
   /**
-   * handleFileChange function is responsible for updating the selectedFile state when a file is chosen using the file input.
-   * It extracts the selected file from the event object and sets it as the new value of the selectedFile state variable,
-   * allowing it to be used for further processing, such as file upload.
+   * handleFileChange function is responsible for updating the selectedFiles state
+   * when one or more files are chosen using the file input.
+   * It extracts the selected files from the event object and sets them as the new value
+   * of the selectedFiles state variable, allowing them to be used for further processing, such as file upload.
    */
   const handleFileChange = (event) => {
-    // Update the selectedFile state with the chosen file
-    setSelectedFile(event.target.files[0]);
+    // Update the selectedFiles state with the chosen files
+    setSelectedFiles([...selectedFiles, ...event.target.files]);
   };
 
   /**
- * handleUpload Function
- *
- * This function handles the file upload logic when the "Uploader" button is clicked. It checks if a file
- * is selected, updates the state with the chosen file
- * If no file is selected, it logs a message to indicate that no file was selected.
- */
-const handleUpload = () => {
-  if (selectedFile) {
-    // Add the selected file to the uploadedFiles state with an initial selectedColumn value of null.
-    setUploadedFiles([...uploadedFiles, { file: selectedFile, selectedColumn: null }]);
-    console.log('Uploading file:', selectedFile);
-    // TODO: Additional file upload logic probably here.
-  } else {
-    // Log a message if no file is selected.
-    console.log('No file selected.');
-  }
-};
+   * handleUpload Function
+   *
+   * This function handles the file upload logic when the "Upload" button is clicked.
+   * It checks if any files are selected, updates the state with the chosen files,
+   * and adds them to the uploadedFiles state with an initial selectedColumn value of null.
+   * If no files are selected, it logs a message to indicate that no files were selected.
+   */
+  const handleUpload = () => {
+    if (selectedFiles.length > 0) {
+      // Add the selected files to the uploadedFiles state with initial selectedColumn values of null.
+      const newUploadedFiles = selectedFiles.map((file) => ({
+        file,
+        selectedColumn: null,
+      }));
+      setUploadedFiles([...uploadedFiles, ...newUploadedFiles]);
+      console.log('Uploading files:', newUploadedFiles);
+      setTableIsVisible(true);
+      // TODO: Additional file upload logic probably here.
+    } else {
+      // Log a message if no files are selected.
+      console.log('No files selected.');
+    }
+
+    // Clear the selectedFiles state after uploading
+    setSelectedFiles([]);
+  };
 
   return (
-  <div>
-    {/* File input */}
-    <input type="file" onChange={handleFileChange} style={{ display: 'block' }} />
-
-    {/* Uploader button, TODO: has some css to display better, might change this to the CSS file */}
-    <button onClick={handleUpload} style={{ display: 'block' }}>Upload</button>
-  </div>
-);
+    <div className="uploader-container">
+      {/* I would like to maybe combine the buttons into one */}
+      <input className={"loader-button"} type="file"  accept=".grc" onChange={handleFileChange}  multiple />
+      <button className={"loader-button"} onClick={handleUpload}>
+        Upload
+      </button>
+    </div>
+  );
 }
-
 /**
  * Selector Component
  *
@@ -67,45 +77,46 @@ const handleUpload = () => {
  *
  * @returns {JSX.Element} The rendered Selector component.
  */
-function Selector({ uploadedFiles, handleSelect }) {
-  return (
-    <div>
-      <h2>Uploaded Files:</h2>
-      {/* Represents an HTML table to display the uploaded files and selection options. */}
-      <table>
-        <thead>
-          <tr>
-            <th>File</th>
-            <th>Select Receiver</th>
-            <th>Select Transmitter</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* This is a JavaScript expression inside curly braces {}
-          that maps over the uploadedFiles array to generate table rows for each uploaded file. */}
-          {uploadedFiles.map((file, index) => (
-            <tr key={index}>
-              <td>{file.file.name}</td>
-              <td>
-                <input
-                  type="radio"
-                  name="receiver"
-                  onChange={() => handleSelect(index, 'TX')}
-                />
-              </td>
-              <td>
-                <input
-                  type="radio"
-                  name="transmiter"
-                  onChange={() => handleSelect(index, 'RX')}
-                />
-              </td>
+function Selector({ uploadedFiles, handleSelect, tableIsVisible }) {
+  if (tableIsVisible) {
+    return (
+        <div>
+          {/* Represents an HTML table to display the uploaded files and selection options. */}
+          <table>
+            <thead>
+            <tr>
+              <th>File</th>
+              <th>Select Receiver</th>
+              <th>Select Transmitter</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+            </thead>
+            <tbody>
+            {/* This is a JavaScript expression inside curly braces {}
+          that maps over the uploadedFiles array to generate table rows for each uploaded file. */}
+            {uploadedFiles.map((file, index) => (
+                <tr key={index}>
+                  <td>{file.file.name}</td>
+                  <td>
+                    <input
+                        type="radio"
+                        name="receiver"
+                        onChange={() => handleSelect(index, 'TX')}
+                    />
+                  </td>
+                  <td>
+                    <input
+                        type="radio"
+                        name="transmiter"
+                        onChange={() => handleSelect(index, 'RX')}
+                    />
+                  </td>
+                </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
+    );
+  }
 }
 
 /**
@@ -116,11 +127,13 @@ function Selector({ uploadedFiles, handleSelect }) {
  * @param {Function} handleSendToSDR      - Function that does the upload process
  * @returns {JSX.Element} - The rendered Sender component.
  */
-function Sender({ selectedFileColumnRX, selectedFileColumnTX, handleSendToSDR }) {
-
+function Sender({ selectedFileColumnRX, selectedFileColumnTX }) {
+  const handleSendToSDR = (rxFile, txFile) => {
+    console.log("Sending RX(" + rxFile.file.name + ") and TX(" + txFile.file.name + ") to SDR!");
+  }
   return (
-    <div>
-      <button onClick={() => handleSendToSDR(selectedFileColumnRX, selectedFileColumnTX)}>Send to Sdr Device</button>
+    <div className={"sender-container"}>
+      <button className={"loader-button"} onClick={() => handleSendToSDR(selectedFileColumnRX, selectedFileColumnTX)}>Send to Sdr Device</button>
     </div>
   );
 }
@@ -134,23 +147,11 @@ export default function Loader() {
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [selectedFileColumnRX, setSelectedFileColumnTX] = useState(null);
   const [selectedFileColumnTX, setSelectedFileColumnRX] = useState(null);
-
-  /**
-   * handleSelect Function
-   *
-   * Handles the selection of receivers and transmitters for uploaded files.
-   *
-   * @param {number} index - The index of the file in the `uploadedFiles` array.
-   * @param {string} column - The selected column identifier ('TX' or 'RX') for the receiver or transmitter.
-   */
+  const [tableIsVisible, setTableIsVisible] = useState(false);
   const handleSelect = (index, column) => {
-    //  creates a new array updatedFiles by spreading the contents of the uploadedFiles array.
-    //  This step ensures that we're creating a new array to avoid directly modifying the state.
     const updatedFiles = [...uploadedFiles];
     updatedFiles[index].selectedColumn = column;
-
     setUploadedFiles(updatedFiles);
-
     if (column === 'RX') {
       setSelectedFileColumnRX(updatedFiles[index]);
     } else if (column === 'TX') {
@@ -158,23 +159,14 @@ export default function Loader() {
     }
   };
 
-  const handleSendToSDR = (rxFile, txFile) => {
-    // TODO: implement this
-    console.log("Sending " + rxFile + " and " + txFile + " to SDR!");
-  }
   return (
-    <section>
-      <Uploader uploadedFiles={uploadedFiles} setUploadedFiles={setUploadedFiles} />
-      <Selector uploadedFiles={uploadedFiles} handleSelect={handleSelect} />
-      <div>
-        <h2>Selected File -  Receiver:</h2>
-        {selectedFileColumnRX ? <p>{selectedFileColumnRX.file.name}</p> : <p>No file selected.</p>}
+    <div className="container">
+      <div className="component-container">
+        <Uploader uploadedFiles={uploadedFiles} setUploadedFiles={setUploadedFiles} tableIsVisible={tableIsVisible} setTableIsVisible={setTableIsVisible}/>
+        <Selector uploadedFiles={uploadedFiles} handleSelect={handleSelect} tableIsVisible={tableIsVisible} />
+        <Sender selectedFileColumnTX={selectedFileColumnTX} selectedFileColumnRX={selectedFileColumnRX} />
       </div>
-      <div>
-        <h2>Selected File - Transmitter:</h2>
-        {selectedFileColumnTX ? <p>{selectedFileColumnTX.file.name}</p> : <p>No file selected.</p>}
-      </div>
-      <Sender selectedFileColumnTX={selectedFileColumnTX} selectedFileColumnRX={selectedFileColumnRX} handleSendToSDR={handleSendToSDR}/>
-    </section>
+    </div>
   );
 }
+

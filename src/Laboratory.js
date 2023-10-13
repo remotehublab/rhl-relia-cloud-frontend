8f9d745e44f20e87d9dad2177c27d2919343b17e
Pedro Amarante <ppedrolia@gmail.com>
2023-10-13 14:04:31 -0700

    Removed effect hook from lab to force relia-widgets to appear

--- src/Laboratory.js
+++ src/Laboratory.js
@@ -20,27 +20,25 @@ import $ from 'jquery';
  * @returns {JSX.Element} The rendered Introduction component.
  */
 function Laboratory({currentSession, setCurrentSession}) {
-    useEffect(() => {
-        window.API_BASE_URL = "/api/";
-        console.log(" im here");
-        if (!currentSession.renderingWidgets && (
-            currentSession.status === 'fully-assigned'
-            || currentSession.status === 'receiver-still-processing'
-            || currentSession.status === "transmitter-still-processing")) {
-            const widgets = new ReliaWidgets($("#relia-widgets"));
-            widgets.start();
-            const newSession = {
-                            "taskIdentifier": currentSession.taskIdentifier,
-                            "status": currentSession.status,
-                            "message": currentSession.message,
-                            "renderingWidgets": true,
-                        }
-            setCurrentSession(newSession);
-            Object.assign(currentSession, newSession);
 
+    window.API_BASE_URL = "/api/";
+    console.log(" im here");
+    if (!currentSession.renderingWidgets && (
+        currentSession.status === 'fully-assigned'
+        || currentSession.status === 'receiver-still-processing'
+        || currentSession.status === "transmitter-still-processing")) {
+        const widgets = new ReliaWidgets($("#relia-widgets"));
+        widgets.start();
+        const newSession = {
+                        "taskIdentifier": currentSession.taskIdentifier,
+                        "status": currentSession.status,
+                        "message": currentSession.message,
+                        "renderingWidgets": true,
+                    }
+        setCurrentSession(newSession);
+        Object.assign(currentSession, newSession);
+    }
 
-        }
-    }, []);
     return (
         <Container className={"loader-container"}>
             <div id={"relia-widgets"}> </div>

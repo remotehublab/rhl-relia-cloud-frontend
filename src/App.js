eee4ddff1a745335c54e977041fb838b58ed3579
Pablo Orduña <pablo@labsland.com>
2024-01-06 14:53:01 -0800

    Use basename to change /static/ and /locales/

--- src/App.js
+++ src/App.js
@@ -6,21 +6,19 @@ import MainIndex from "./MainIndex.js";
 import Development from "./Development.js";
 import Loader from "./Loader.js";
 import Login from "./login.js";
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Outerloader from "./Outerloader";
 
 function App() {
   return (
-    <Router>
-
-    <Routes>
-        <Route exact path='/login' exact element={<Login />} />
-        <Route exact path='/' exact element={<MainIndex />} />
-        <Route exact path='/loader' exact element={<Loader />} />
-        <Route path='/dev' element={<Development/>} />
-        <Route path='/outerloader' element={<Outerloader/>} />
-    </Routes>
+    <Router basename={process.env.REACT_APP_BASE_URL}>
+      <Routes>
+          <Route exact path='/login' exact element={<Login />} />
+          <Route exact path='/' exact element={<MainIndex />} />
+          <Route exact path='/loader' exact element={<Loader />} />
+          <Route path='/dev' element={<Development/>} />
+          <Route path='/outerloader' element={<Outerloader/>} />
+      </Routes>
     </Router>
   );
 }

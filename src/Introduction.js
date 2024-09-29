a53e2516f5f24e4dbfb747ea6905e0ac09f8c659
Pablo Orduña <pablo@labsland.com>
2024-09-28 19:33:33 -0700

    Change intro for red pitaya

--- src/Introduction.js
+++ src/Introduction.js
@@ -23,8 +23,13 @@ function Introduction() {
             <p>Welcome to RELIA, the Software-defined Radio (SDR) remote laboratory.</p>
             <p>The steps to use the remote laboratory are:</p>
             <ol>
-                <li>In <a href="https://www.gnuradio.org/" target="_blank">GNU Radio</a>, you can build two files, one for a transmitter and one for a receiver. 
-                    You can use blocks that are QT widgets (Frequency Sink, Vector Sink, and others), as well as Pluto SDR blocks (PlutoSDR Source and PlutoSDR Sink). You can find an <a href="https://raw.githubusercontent.com/remotehublab/rhl-relia-gr-runner/main/examples/test_tx.grc" target="_blank">example transmitter</a> and an <a href="https://raw.githubusercontent.com/remotehublab/rhl-relia-gr-runner/main/examples/test_rx.grc" target="_blank">example receiver file</a>.</li>
+                <li>In <a href="https://www.gnuradio.org/" target="_blank">GNU Radio</a>, you can build two files, one for a transmitter and one for a receiver. </li>
+                {deviceName === "ADALM Pluto" && (
+                    <li>You can use blocks that are QT widgets (Frequency Sink, Vector Sink, and others), as well as Pluto SDR blocks (PlutoSDR Source and PlutoSDR Sink). You can find an <a href="https://raw.githubusercontent.com/remotehublab/rhl-relia-gr-runner/main/examples/test_tx.grc" target="_blank">example transmitter</a> and an <a href="https://raw.githubusercontent.com/remotehublab/rhl-relia-gr-runner/main/examples/test_rx.grc" target="_blank">example receiver file</a>.</li>
+                )}
+                {deviceName === "Red Pitaya" && (
+                    <li>You can use blocks that are QT widgets (Frequency Sink, Vector Sink, and others), as well as Red Pitaya SDR blocks (RedPitayaSDR Source and RedPitayaSDR Sink). You can find an <a href="https://raw.githubusercontent.com/remotehublab/rhl-relia-gr-runner/refs/heads/main/examples/test_tx_redpitaya.grc" target="_blank">example transmitter</a> and an <a href="https://raw.githubusercontent.com/remotehublab/rhl-relia-gr-runner/refs/heads/main/examples/test_rx_redpitaya.grc" target="_blank">example receiver file</a>.</li>
+                )}
                 <li>In the remote laboratory, click on Load files, and upload the two files. Then select which one will run in the transmitter and which one in the receiver. Then click on Send to SDR devices.</li>
                 <li>Then, you will be assigned to one of the SDR setups that has both a transmitter and a receiver. You will see them in real-time running your two blocks for a few seconds. You will see the results of the widgets and the camera pointing at them.</li>
             </ol>
